bba28532c6fc7c62a605c17e9a5508c635025a7d
syota-nobori <102848155+syota-nobori@users.noreply.github.com>
2022-04-06 16:31:28 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,132 @@
+/**
+ * const,ret等変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // retは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // retは再宣言不可能
+// let val2 = "let変数を再宣言"；
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // const変数は上書き不可
+// // val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言"
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "翔太",
+//   age: "28"
+// };
+// val4.name = "sta";
+// val4.address = "tokyo";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monekey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "翔太";
+// const age = 28;
+// // 「私の名前は翔太です。年齢は２８歳です。」
+
+// // 従来の方法
+// const message1 = "私の名前は"　+ name + "です。年齢は"　+ age + "です";
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数　 *str=組み込み関数　　任意のデータを文字列に変換できる。
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// }
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => num1 + num2;
+
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "翔太",
+//   age: 28
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = [`翔太`, 28];
+
+// const message = `名前は${myProfile[0]}です。年齢は${myProfile[1]}際です。`;
+// console.log(message);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello("翔太");
+
+/**
+ * スプレット構文　...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
